be4f63de7800c5974d87c737d3051d98d263a384
Nazareh <nazarehturmina@gmail.com>
2018-06-01 13:52:25 +0800

    Add Dependency Injection using Spring Framework.

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework;
 
+import guru.springframework.controllers.ConstructorInjectedController;
 import guru.springframework.controllers.MyController;
+import guru.springframework.controllers.PropertyInjectedController;
+import guru.springframework.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +15,10 @@ public class DiDemoApplication {
 		ApplicationContext applicationContext =  SpringApplication.run(DiDemoApplication.class, args);
 		MyController controller = (MyController) applicationContext.getBean("myController");
 		controller.hello();
+
+		System.out.println(applicationContext.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(applicationContext.getBean(SetterInjectedController.class).sayHello());
+		System.out.println(applicationContext.getBean(ConstructorInjectedController.class).sayHello());
 
 	}
 }
